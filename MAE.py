4e5c489f1468d582b69fdc997d45904250fb15ce
David restrepo <57241831+dsrestrepo@users.noreply.github.com>
2024-09-25 17:10:08 -0500

    Update test function

--- MAE.py
+++ MAE.py
@@ -16,6 +16,20 @@ from torch.jit import Final
 
 from timm.layers import Mlp, DropPath, use_fused_attn
 
+class LayerScale(nn.Module):
+    def __init__(
+            self,
+            dim: int,
+            init_values: float = 1e-5,
+            inplace: bool = False,
+    ) -> None:
+        super().__init__()
+        self.inplace = inplace
+        self.gamma = nn.Parameter(init_values * torch.ones(dim))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.mul_(self.gamma) if self.inplace else x * self.gamma
+
 class Attention(nn.Module):
     fused_attn: Final[bool]
 
--- run_test_output.py
+++ run_test_output.py
@@ -6,10 +6,31 @@ import os
 import csv
 from tqdm import tqdm
 
-################ Read Dataset ################
-df_test = pd.read_csv('..//data/X_test.csv')
-print(f'Test values shape: {df_test.shape}')
+import warnings
+warnings.filterwarnings("ignore")
 
+import joblib
+import xgboost
+
+def load_xgboost_model(filename):
+    # Load the model from the file
+    loaded_model = joblib.load(filename)
+    print(f"Model loaded from {filename}")
+    return loaded_model
+
+def predict_in_batches(model, X, batch_size=64):
+    """Perform batch predictions using the provided model."""
+    predictions = []
+    # Split the data into batches
+    for X_batch in np.array_split(X, len(X) // batch_size + 1):
+        # Perform prediction on each batch
+        predictions.append(model.predict(X_batch))
+    # Concatenate all batch predictions into a single array
+    return np.concatenate(predictions, axis=0)
+
+################ Read Dataset ################
+df_test = pd.read_csv('../data/X_test.csv')
+print(f'Test values shape: {df_test.shape}')
 
 ################ Clean Missing Data ################
 def clean_missing(df, threshold=20 + 3, missing_per_col=100, cols_to_remove=None):
@@ -46,6 +67,9 @@ missing_per_col = 500
 
 df_test, _ = clean_missing(df_test, missing_per_row, cols_to_remove=[])
 
+# get a subset of the first 100000 rows
+df_test = df_test[:100000]
+
 
 # Create a list of columns to ignore
 columns_ignore = ['first_race', 'chartyear', 'hadm_id']
@@ -65,11 +89,11 @@ depth=8
 decoder_depth=4
 num_heads=8
 mlp_ratio=4.0
-
+device = 'mps'
 
 imputer = ReMaskerStep(dim=columns, mask_ratio=mask_ratio, max_epochs=max_epochs, save_path=save_path, batch_size=batch_size,
                       embed_dim=embed_dim, depth=depth, decoder_depth=decoder_depth, num_heads=num_heads, mlp_ratio=mlp_ratio,
-                      weights=weights)
+                      weights=weights, device=device)
 
 
 with open('100_Labs_Train_0.25Mask_L_V3/norm_parameters.pkl', 'rb') as file:
@@ -79,14 +103,21 @@ imputer.norm_parameters = loaded_norm_parameters
 
 
 ################ Test the model ################
-def test_model(imputer, df_test, exclude_columns=[], eval_batch_size=2048, predictions_file='results_test_predictions.csv'):
+def test_model(imputer, df_test, exclude_columns=[], eval_batch_size=32, predictions_file='results_test_predictions_test_100k.csv', ignore_previous=True, baseline=True):
     # Initialize the DataFrame to store predictions
     df_predictions = df_test.copy()
     
+    ItemID = 0
     for column, column_name in enumerate(tqdm(df_test.columns, desc="Processing columns")):
         # Columns with time
         if 'time' in column_name:
             continue
+        
+        # Ignore columns with '_last' in the name
+        if ignore_previous:
+            if '_last' in column_name:
+                continue
+        
         # Columns to ignore
         if column_name in exclude_columns:
             continue
@@ -101,14 +132,53 @@ def test_model(imputer, df_test, exclude_columns=[], eval_batch_size=2048, predi
         # Mask all values in that column with NaN
         X_test_masked.iloc[:, column] = np.nan
 
+        ################ Impute the values using MAE ################
+        print(f'Imputing column: {column_name} using MAE')
+        
         # Impute the values:
         X_test_imputed = imputer.transform(X_test_masked, eval_batch_size=eval_batch_size).cpu().numpy()
         
-        # Fill the masked column with the imputed values
-        df_predictions.loc[df_test[column_name].notna(), column_name] = X_test_imputed[:, column]
+        # Add a column to the DataFrame with the imputed values
+        imputed_column_name = column_name + '_imputed_MAE'
+        df_predictions[imputed_column_name] = np.nan
+        
+        # Create a new column with the imputed values from the MAE model
+        df_predictions.loc[df_test[column_name].notna(), imputed_column_name] = X_test_imputed[:, column]
+        
+        
         
         # Save the predictions column to the CSV file
-        df_predictions[[column_name]].to_csv(predictions_file, mode='a', header=False, index=False)
+        #df_predictions[[imputed_column_name]].to_csv(predictions_file, mode='a', header=False, index=False)
+        
+        ################ Impute the values using the baseline ################
+        # Impute the values:
+        if baseline:
+            print(f'Imputing column: {X_test_masked.columns[ItemID*2]} using the xgboost model')
+            baseModel = load_xgboost_model(f"/Users/davidrestrepo/MAE Tabular/xgboost/{ItemID}best_model.pkl")
+            
+            # Drop the column to be imputed
+            X_test_masked_copy = X_test_masked.drop(X_test_masked.columns[ItemID*2], axis=1).copy()
+            
+            # Perform batch predictions using the batch size
+            #X_test_imputed = predict_in_batches(baseModel, X_test_masked_copy, batch_size=eval_batch_size)
+            X_test_imputed = baseModel.predict(X_test_masked_copy)
+            
+            # Add a column to the DataFrame with the imputed values
+            imputed_column_name = column_name + '_imputed_xgb'
+            df_predictions[imputed_column_name] = np.nan
+            
+            # Create a new column with the imputed values from the MAE model
+            df_predictions.loc[df_test[column_name].notna(), imputed_column_name] = X_test_imputed
+            
+            # Save the predictions column to the CSV file
+            #df_predictions[[imputed_column_name]].to_csv(predictions_file, mode='a', header=False, index=False)
+            
+            ItemID += 1
+        
+        # Fill the masked column with the imputed values
+        #df_predictions.loc[df_test[column_name].notna(), column_name] = X_test_imputed[:, column]
+        # Save the predictions column to the CSV file
+        #df_predictions[[column_name]].to_csv(predictions_file, mode='a', header=False, index=False)
         
     # Save the final DataFrame to CSV
     df_predictions.to_csv(predictions_file, index=False)
